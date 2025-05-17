ce94f6484026627e46d5d0fe1bb36972f7e0ef10
brandon-alexis <brandonmachado159@gmail.com>
2025-05-17 18:16:37 -0500

    modificacion de volumen bajo

--- src/jugadores/Duende.java
+++ src/jugadores/Duende.java
@@ -162,7 +162,7 @@ public class Duende {
             if (this.detectarColisionComida(comida)) {
                 it.remove();
                 Juego.incrementarPuntaje();
-                this.sonido.reproducirSonidoMultiple("moneda", 0.7f);
+                this.sonido.reproducirSonidoMultiple("moneda", 0.6f);
             }
         }
     }
